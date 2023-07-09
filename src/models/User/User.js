b7c5d53fa07f4c95cd0f078268ef6db1348f129a
Douglas Martins <douglas5422@outlook.com>
2023-07-09 16:48:21 -0300

    fix: correção na busca de todos os usuários

--- src/models/User/User.js
+++ src/models/User/User.js
@@ -80,6 +80,7 @@ class User {
     async allUsersDB() {
         try {
             this.results = await UserSchema.findAll({
+                where: {user_ativo: 'S'},
                 order: [
                     ['user_date_create', 'ASC']
                 ]
